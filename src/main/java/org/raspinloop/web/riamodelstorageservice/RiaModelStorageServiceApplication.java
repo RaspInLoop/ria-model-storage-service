3cc91d2c881fec3f069cdaaa80d220f5b6a19cc7
Fred <fmahiant@gmail.com>
2019-10-21 20:38:09 +0200

    Skeleton

---
+++ src/main/java/org/raspinloop/web/riamodelstorageservice/RiaModelStorageServiceApplication.java
@@ -0,0 +1,13 @@
+package org.raspinloop.web.riamodelstorageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiaModelStorageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiaModelStorageServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/raspinloop/web/riamodelstorageservice/RiaModelStorageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.raspinloop.web.riamodelstorageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiaModelStorageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
